d9ce78edc1f9561370c0ad07fe93d393b78a18d6
AlecMbanga <1653526@students.wits.ac.za>
2019-08-09 02:34:14 +0200

    main page button linked to play area intent

--- app/src/main/java/com/example/natchess/MainActivity.java
+++ app/src/main/java/com/example/natchess/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.natchess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPlayIntent();
+            }
+        });
+    }
+
+    public void openPlayIntent(){
+        Intent intent = new Intent(this, play.class);
+        startActivity(intent);
     }
 }
